ca575a3913dbb288d5cb7be4facfc99e4901a6c9
zodang <78644385+zodang@users.noreply.github.com>
2025-11-10 23:40:24 +0900

    Update 모험가길드.cpp

--- 이코테/그리디/모험가길드.cpp
+++ 이코테/그리디/모험가길드.cpp
@@ -1,36 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int N;
-    int S[100000];
-    
-    int result = 0;
-    
-    // Input
-    cin >> N;
-    for(int i = 0; i < N; i++)
-    {
-        cin >> S[i];
-    }
-    
-    for (int i = 1; i <= N; i++)
-    {
-        int count = 0;
-        
-        for(int j = 0; j < N; j++)
-        {
-            if (S[j] == i)
-            {
-                count++;
-            }
-        }
-        
-        result += count / i;
-    }
-    
-    cout << result;
+int N;
+vector<int> vec;
 
-    return 0;
+int main() {
+    cin >> N;
+    
+    for (int i = 0; i < N; i++)
+    {
+        int x;
+        cin >> x;
+        vec.push_back(x);
+    }
+
+    sort(vec.begin(), vec.end());
+    
+    int guildCount = 0;
+    int currentSum = 0;
+    
+    for (int i = 0; i < N; i++)
+    {
+        currentSum++;
+        
+        // 최소 길드원 확보
+        if (currentSum >= vec[i])
+        {
+            guildCount++;
+            currentSum = 0;
+        }
+    }
+    
+    cout << guildCount;
 }
