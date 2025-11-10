bf7bddc4c3f671416fa6eda64086ee39864e085f
zodang <78644385+zodang@users.noreply.github.com>
2025-11-11 01:52:22 +0900

    Update 곱하기혹은더하기.cpp

--- 이코테/그리디/곱하기혹은더하기.cpp
+++ 이코테/그리디/곱하기혹은더하기.cpp
@@ -1,36 +1,21 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    string S;
+string S;
+
+int main() {
+    
     cin >> S;
     
-    int result = 0;
+    int result = S[0] -'0';
     
-    for(int i = 0; i < S.length(); i++)
+    for (int i = 1; i < S.size(); i++)
     {
-        int value = S[i] - '0';
-        
-        // 0 혹은 1일 때는 곱하는 것보다 더하는 것이 효율적
-        if (value <= 1)
-        {
-            result += value;
-            
-        }
-        else
-        {
-            // 첫 문자가 0일 때의 예외처리
-            if (result == 0)
-            {
-                result = 1;
-            }
-            
-            result *= value;
-        }
+        int currentNum = S[i] -'0';
+
+        if (result <= 1 || currentNum <= 1) result += currentNum;
+        else result *= currentNum;
     }
     
-  cout << result;
-  return 0;
+    cout << result;
 }
