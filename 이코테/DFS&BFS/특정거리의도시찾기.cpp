a8b5bfb706fb16e7bd35b3da3f405c7392d8f839
조다은 <78644385+zodang@users.noreply.github.com>
2026-01-13 18:45:43 +0900

    Update 특정거리의도시찾기.cpp
    특정거리의도시찾기 2회차 풀이

--- 이코테/DFS&BFS/특정거리의도시찾기.cpp
+++ 이코테/DFS&BFS/특정거리의도시찾기.cpp
@@ -1,39 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 1e9;
 
+const int INF = 1e9;
 int N, M, K, X;
-vector<int> graph[300001];
+
+vector<vector<int>> graph;
+bool visited[300001];
 int d[300001];
 
 void bfs(int start)
 {
     queue<int> q;
+    
     q.push(start);
+    visited[start] = true;
     d[start] = 0;
     
-    while(!q.empty())
+    while (!q.empty())
     {
-        int currentN = q.front();
+        int currentIdx = q.front();
+        
         q.pop();
         
-        for (int i = 0; i < graph[currentN].size(); i++)
+        for (int i = 0; i < graph[currentIdx].size(); i++)
         {
-            if (d[graph[currentN][i]] == INF)
-            {
-                d[graph[currentN][i]] = d[currentN] + 1;
-                q.push(graph[currentN][i]);
-            }
+            int nearIdx = graph[currentIdx][i];
+            if (visited[nearIdx]) continue;
+            
+            q.push(nearIdx);
+            visited[nearIdx] = true;
+            d[nearIdx] = d[currentIdx] + 1;
         }
     }
 }
 
 int main() {
     
-    // start부터 n번 노드까지의 최소거리 초기화
-    fill_n(d, 300001, INF);
-    
+    // 1. 데이터 초기화
 	cin >> N >> M >> K >> X;
+	
+	graph.resize(N+1);
+	fill_n(visited, N+1, 0);
+	fill_n(d, N+1, 0);
 	
 	for (int i = 0; i < M; i++)
 	{
@@ -42,17 +50,21 @@ int main() {
 	    graph[a].push_back(b);
 	}
 	
+	// 2. bfs 실행
 	bfs(X);
 	
-	int totalCount = 0;
-	for (int i = 1; i <= N; i++)
+	// 3. 최소거리가 K인 노드 출력력
+	int count = 0; 
+	
+	for (int i = 1; i < N+1; i++)
 	{
 	    if (d[i] == K) 
 	    {
-	        totalCount++;
+	        count++;
 	        cout << i << '\n';
 	    }
 	}
-	
-	if (totalCount == 0) cout << -1;
+
+    if (count == 0) cout << -1 << '\n';
 }
+
