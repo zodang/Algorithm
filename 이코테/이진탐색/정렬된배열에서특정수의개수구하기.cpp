59a8cf1e9335103253dcf1616580fea14d11d0a0
조다은 <78644385+zodang@users.noreply.github.com>
2026-02-03 13:10:42 +0900

    Update 정렬된배열에서특정수의개수구하기.cpp
    정렬된배열에서특정수의개수구하기 2회차 풀이

--- 이코테/이진탐색/정렬된배열에서특정수의개수구하기.cpp
+++ 이코테/이진탐색/정렬된배열에서특정수의개수구하기.cpp
@@ -4,22 +4,44 @@ using namespace std;
 int N, x;
 vector<int> arr;
 
-int main(void)
+int lower_bound(int startIdx, int endIdx, int target)
 {
-    cin >> N >> x;
-    
-    for (int i = 0; i < N; i++)
+    while (startIdx < endIdx)
     {
-        int t;
-        cin >> t;
-        arr.push_back(t);
+        int midIdx = (startIdx + endIdx) / 2;
+        
+        if (arr[midIdx] < target) startIdx = midIdx + 1;
+        else endIdx = midIdx;
     }
     
-    int lowerIdx = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
-    int upperIdx = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+    return startIdx;
+}
+
+int upper_bound(int startIdx, int endIdx, int target)
+{
+    while (startIdx < endIdx)
+    {
+        int midIdx = (startIdx + endIdx) / 2;
+        
+        if (arr[midIdx] <= target) startIdx = midIdx + 1;
+        else endIdx = midIdx;
+    }
     
-    int count = upperIdx - lowerIdx;
+    return startIdx;
+}
+
+int main() {
+	cin >> N >> x;
+	
+	for (int i = 0; i < N; i++)
+	{
+	    int a;
+	    cin >> a;
+	    arr.push_back(a);
+	}
+	
+	int leftIdx = lower_bound(0, N-1, x);
+    int rightIdx = upper_bound(0, N-1, x);
     
-    if (count == 0) cout << -1;
-    else cout << count;
+    cout << leftIdx << ' ' << rightIdx;
 }
