e0d150e6cbfe6ae7ace242aea629efb1b9088466
조다은 <78644385+zodang@users.noreply.github.com>
2026-02-05 22:59:08 +0900

    Update 공유기설치.cpp
    공유기설치 2회차 풀이

--- 이코테/이진탐색/공유기설치.cpp
+++ 이코테/이진탐색/공유기설치.cpp
@@ -4,45 +4,41 @@ using namespace std;
 int N, C;
 vector<int> arr;
 
-int binarySearch(int startD, int endD)
+bool get_wifi_count(int interval)
 {
-    // 인접한 두 공유기의 최대 거리
-    int result = 0;
+    int count = 1;
+    int lastIdx = 0;
     
-    while (startD <= endD)
+    // 2. 중간거리 이상을 만족하는 곳마다 설치
+    for (int i = 1; i < arr.size(); i++)
     {
-        // midD를 넘는 곳마다 공유기 설치
-        int midD = (startD + endD) / 2;
-        int count = 1;
-        int wifiPos = arr[0];
-        
-        for (int i = 0; i < N; i++)
+        if (arr[i] - arr[lastIdx] >= interval)
         {
-            if (arr[i] - wifiPos >= midD)
-            {
-                wifiPos = arr[i];
-                count++;
-            }
-        }
-        
-        // 공유기 개수 미달 → endD 줄여서 재탐색
-        if (count < C) endD = midD - 1;
-        
-        // 공유기 개수 충족 → startD 늘려서 재탐색
-        else
-        {
-            startD = midD + 1;
-            result = midD;
+            count += 1;
+            lastIdx = i;
         }
     }
     
-    return result;
+    return count >= C;
+}
+
+int binary_search(int min_interval, int max_interval)
+{
+    if (min_interval > max_interval) return max_interval;
+    
+    // 1. 시작점+1과 끝지점의 중간 거리 구하기
+    int mid_interval = (min_interval + max_interval) / 2;
+    
+    // 3. 설치 개수가 n개를 넘으면 중간지점 ~ 끝지점의 중간거리
+    if (get_wifi_count(mid_interval)) return binary_search(mid_interval + 1, max_interval);
+    
+    // 4. 설치 개수가 n개를 못넘으면 시작점 + 1 ~ 중간지점까지의 중간거리
+    else return binary_search(min_interval, mid_interval-1);
 }
 
 int main(void)
 {
     cin >> N >> C;
-    
     for (int i = 0; i < N; i++)
     {
         int x;
@@ -52,8 +48,5 @@ int main(void)
     
     sort(arr.begin(), arr.end());
     
-    int minD = 1;
-    int maxD = arr[N-1] - arr[0];
-    
-    cout << binarySearch(minD, maxD);
+    cout << binary_search(1, arr[N-1] - arr[0]);
 }
