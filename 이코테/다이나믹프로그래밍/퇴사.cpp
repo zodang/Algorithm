50c65d719c8fa300fcf39f1df79b61d92794b553
조다은 <78644385+zodang@users.noreply.github.com>
2026-02-10 17:30:50 +0900

    Update 퇴사.cpp
    퇴사 2회차 풀이

--- 이코테/다이나믹프로그래밍/퇴사.cpp
+++ 이코테/다이나믹프로그래밍/퇴사.cpp
@@ -2,35 +2,42 @@
 using namespace std;
 
 int N;
-vector<pair<int, int>> arr;
+int T[16] = {0};
+int P[16] = {0};
+int d[16] = {0};
 
-int main(void)
-{
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        int t;
-        int p;
-        cin >> t >> p;
-        
-        arr.push_back(make_pair(t, p));
-    }
-    
-    vector<int> dp(N + 1, 0);
-    int maxP = 0;
-    
-    for (int i = N - 1; i >= 0; i--)
-    {
-        int nextIdx = i + arr[i].first;
-        
-        if (nextIdx <= N) 
-        {
-            dp[i] = max(maxP, arr[i].second + dp[nextIdx]);
-            maxP = dp[i];
-        }
-        
-        else dp[i] = maxP;
-    }
-    
-    cout << maxP;
+int main() {
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++)
+	{
+	    int t, p;
+	    cin >> t >> p;
+	    T[i] = t;
+	    P[i] = p;
+	}
+	
+	int max_sum = 0;
+	
+	for (int i = N; i > 0; i--)
+	{
+	    // 1. 퇴사일 초과 여부 확인
+	    if (i + T[i] > N+1)
+	    {
+	        d[i] = max_sum;
+	        continue;
+	    }
+	    
+	    // 2. 최대 이익과 비교
+	    int next_time = i + T[i]; 
+	    
+	    if (d[next_time] + P[i] > max_sum)
+	    {
+	        d[i] = d[next_time] + P[i];
+	        max_sum = d[i];
+	    }
+	    else d[i] = max_sum;
+	}
+	
+	cout << max_sum;
 }
