7a0cd17c814f132510783b30570fd4e50efa0d92
조다은 <78644385+zodang@users.noreply.github.com>
2026-02-20 00:28:45 +0900

    Update 플로이드.cpp
    플로이드 2회차 풀이

--- 이코테/최단경로/플로이드.cpp
+++ 이코테/최단경로/플로이드.cpp
@@ -2,53 +2,54 @@
 using namespace std;
 
 const int INF = 1e9;
-int n, m;
-int graph[101][101];
 
-int main() 
-{
+int n, m;
+int arr[101][101];
+
+int main() {
+    
     cin >> n;
     cin >> m;
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n+1; i++)
     {
-        fill_n(graph[i], n + 1, INF);
-    }
-    
-    for (int i = 1; i <= m; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        graph[a][b] = min(graph[a][b], c);
-    }
-    
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n+1; j++)
         {
-            if (i == j) graph[i][j] = 0;
+            arr[i][j] = INF;
         }
     }
     
-    for (int k = 1; k <= n; k++)
+    for (int i = 0; i < n+1; i++)
     {
-        for (int i = 1; i <= n; i++)
+        arr[i][i] = 0;
+    }
+    
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        arr[a][b] = min(arr[a][b], c);
+    }
+    
+    for (int k = 1; k < n+1; k++)
+    {
+        for (int i = 1; i < n+1; i++)
         {
-            for (int j = 1; j <= n; j++)
+            for (int j = 1; j < n+1; j++)
             {
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+                arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
             }
         }
     }
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i < n+1; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j < n+1; j++)
         {
-            if (graph[i][j] == INF) cout << 0 << ' ';
-            else cout << graph[i][j] << ' ';
+            if (arr[i][j] >= INF) cout << 0 << ' ';
+            else cout << arr[i][j] << ' ';
         }
-        
         cout << '\n';
     }
 }
